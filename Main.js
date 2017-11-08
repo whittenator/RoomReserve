d912ecbbe2b9f3b57320183355e271b545be9a85
whittenator <twhitten@mix.wvu.edu>
2017-11-08 08:20:18 -0500

    first commit

---
+++ Main.js
@@ -0,0 +1,39 @@
+
+const btnLogin = document.getElementById("btnLogin");
+const btnSignUp = document.getElementById("btnSignUp");
+const txtEmail = document.getElementById("txtEmail");
+const txtPassword = document.getElementById("txtPassword");
+const auth = firebase.auth();
+
+
+btnLogin.addEventListener('click', e => {
+    //Get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email,pass);
+    promise.catch(e => console.log(e.message));
+    
+});
+
+//add signup event
+btnSignUp.addEventListener('click', e => {
+    //Get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //Sign in
+    const promise = auth.createUserWithEmailAndPassword(email,pass);
+    promise.catch(e => console.log(e.message));
+    
+});
+
+//Add a realtime listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+        console.log(firebaseUser);
+    }else{
+        console.log('Not logged in');
+    }
+});
