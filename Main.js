369f223429ac00b9c3a5a77da4c0edfe8f13fa53
whittenator <twhitten@mix.wvu.edu>
2017-11-09 11:15:57 -0500

    Add files via upload
    Added in a small non-function calendar. Need to add in the functionality.

--- Main.js
+++ Main.js
@@ -16,6 +16,8 @@ btnLogin.addEventListener('click', e => {
     const promise = auth.signInWithEmailAndPassword(email,pass);
     promise.catch(e => console.log(e.message));
     
+    
+    
 });
 
 //add signup event
@@ -34,7 +36,19 @@ btnSignUp.addEventListener('click', e => {
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser){
         console.log(firebaseUser);
+        console.log("Logged in");
     }else{
         console.log('Not logged in');
     }
+});
+
+btnLogOut.addEventListener('click', e => {
+    firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+        console.log("User Signed Out!");
+}).catch(function(error) {
+  // An error happened.
+        console.log(error);
+}); 
+    
 });
